5a667a579639c7762970af094f544f2307d829a9
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-23 16:01:41 +0200

    database seed, homepage, about-us

---
+++ resources/views/about/about-us.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about-us">About</a>
+    </nav>
+
+    <h1>About Us</h1>
+
+    <p>It’s finger-lickin’ good. You are such a nerd. No wonder you only hang out with boys. Don’t take it so personally, okay? I don’t like most people. He’s in the vast majority. Do you know anything about sensory deprivation tanks? Specifically how to build one?</p>
+
+    <p>Yeah, I want a date with Bo Derek. We all want things. You’re going to be home by 8, listening to the Talking Heads and reading Vonnegut or something. That sounds like a nice night. Friends don't lie. You act like you want me to be your friend and then you treat me like garbage.</p>
+    
+</body>
+</html>
---
+++ resources/views/index/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about-us">About</a>
+    </nav>
+
+    <h1>Hello mate!</h1>
+
+    <p>Bacon ipsum dolor amet salami chislic sirloin cupim. Burgdoggen capicola hamburger venison tenderloin landjaeger leberkas salami porchetta. Brisket spare ribs kielbasa, doner alcatra picanha capicola t-bone ribeye pork chop corned beef pastrami pork loin. Pork landjaeger burgdoggen chicken corned beef tongue buffalo kielbasa tri-tip pork loin. Shank ball tip kevin landjaeger filet mignon picanha alcatra drumstick bresaola leberkas ribeye. Boudin ribeye salami, turkey kielbasa tri-tip pork chop capicola kevin. Tenderloin kielbasa pork loin tongue corned beef turducken, boudin tail chuck bresaola filet mignon tri-tip beef cow swine.</p>
+
+    <p>Prosciutto chuck leberkas porchetta meatloaf. Sirloin hamburger bacon beef ribs, beef shoulder tri-tip. Chislic turkey pork belly sirloin jowl. Ribeye picanha pork loin filet mignon. Pig ribeye pork loin beef.</p>
+    
+</body>
+</html>
