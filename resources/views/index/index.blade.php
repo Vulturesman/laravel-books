41867272bb7d7ad7af2523881255a18314e37b59
Jakub Kozler <kozlerjakub@gmail.com>
2023-10-24 14:10:12 +0200

    added pluck

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -6,6 +6,7 @@
 
 
     <h1>Hello mate!</h1>
+    <h2>This is a book shop!</h2>
 
     <p>Bacon ipsum dolor amet salami chislic sirloin cupim. Burgdoggen capicola hamburger venison tenderloin landjaeger leberkas salami porchetta. Brisket spare ribs kielbasa, doner alcatra picanha capicola t-bone ribeye pork chop corned beef pastrami pork loin. Pork landjaeger burgdoggen chicken corned beef tongue buffalo kielbasa tri-tip pork loin. Shank ball tip kevin landjaeger filet mignon picanha alcatra drumstick bresaola leberkas ribeye. Boudin ribeye salami, turkey kielbasa tri-tip pork chop capicola kevin. Tenderloin kielbasa pork loin tongue corned beef turducken, boudin tail chuck bresaola filet mignon tri-tip beef cow swine.</p>
 
